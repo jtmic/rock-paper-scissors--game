6130ddef3e7104bb2296adbd7a608c5fdb4c4f73
Jack Mickel <121540283+jtmic@users.noreply.github.com>
2023-01-04 22:24:42 +0000

    Initial commit

--- main.py
+++ main.py
@@ -32,26 +32,19 @@ game_images = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 print(game_images[user_choice])
 
-computer_output = random.randint(0, 2)
+computer_choice = random.randint(0, 2)
 print("Computer chose:")
-print(game_images[computer_output])
+print(game_images[computer_choice])
 
-if user_choice >=3 or user_choice < 0: 
-  print("You chose an invalid number, YOU LOSE!")
-else:
-  print(game_images[user_choice])        
-
-  if user_choice == 0 and computer_output == 2:
-    print("You win!")
-  elif computer_output == 0 and user_choice == 2:
-    print("You Lose!")
-  elif computer_output > user_choice:
-    print("You lose!")
-  elif user_choice >computer_output:
-    print("You Win!")
-  elif computer_output == user_choice:
-    print("It's a draw... CATS GAME!")
-  
-
-
-
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
